15b9e3fb45e97444617c1109d494edf9924862b8
tinaamz198 <tinaamz198@gmail.com>
2026-06-01 15:14:44 +0600

    update

--- first.java
+++ first.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Привет! Мой новый проект работает!");
     System.out.println("Привет! Мой новый проект работает!"); 
+    
+    System.out.println("Привет! Мой новый проект работает!"); 
     }
 }
