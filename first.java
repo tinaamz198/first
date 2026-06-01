3e44e2c971f6f8c57ca9f98aa2e7d5c89e1f22d5
tinaamz198 <tinaamz198@gmail.com>
2026-06-01 13:09:06 +0600

    изменения в добавлкний строки

--- first.java
+++ first.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Привет! Мой новый проект работает!");
+    System.out.println("Привет! Мой новый проект работает!"); 
     }
 }
